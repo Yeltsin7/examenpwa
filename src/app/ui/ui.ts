fd574a6ef49c002e1da9e355db8d9aa147b7a4ab
Yeltsin7 <yelsinlov@gmail.com>
2025-09-22 13:27:06 -0400

    feat(ui): showcase template components on /ui

---
+++ src/app/ui/ui.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui',
+  standalone: true,
+  imports: [],
+  templateUrl: './ui.html',
+  styleUrl: './ui.scss'
+})
+export class Ui {
+
+}
